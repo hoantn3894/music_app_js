c97b54e0d784af6710142256b9cbfc39f45a6c15
hoantn3894 <hoantn.f8@gmail.com>
2021-04-24 17:30:27 +0700

    push source code music app

---
+++ assets/js/future.js
@@ -0,0 +1,10 @@
+// 1. Render songs --> Ok
+// 2. Scroll top --> Ok
+// 3. Play / Pause / Seek --> Ok
+// 4. CD Rotate
+// 5. Next / Prev
+// 6. Random
+// 7. Next / Repeat when ended
+// 8. Active song
+// 9. Scroll active song into view // fix lỗi nearest khi scroll
+// 10. Play song when click
